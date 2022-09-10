7c3dff15f4f99b5c9199401a63eca3068d6dab1c
alex <kasperskiy.94@gmail.com>
2022-09-10 16:08:18 +0300

    fourth day

--- src/js/script.js
+++ src/js/script.js
@@ -5,3 +5,12 @@ burger.addEventListener('click', function() {
     burger.classList.toggle('active');
     menu.classList.toggle('active');
 });
+
+/* Slider */
+
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        spedd: 1200,
+        adaptiveHeight: true,
+    });
+  });
