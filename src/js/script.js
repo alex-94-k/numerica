13b6bb4ca6703c699d6ec194831cfa314f20ae2c
alex <kasperskiy.94@gmail.com>
2022-09-13 22:34:37 +0300

    fifth day

--- src/js/script.js
+++ src/js/script.js
@@ -10,7 +10,18 @@ burger.addEventListener('click', function() {
 
 $(document).ready(function(){
     $('.carousel__inner').slick({
-        spedd: 1200,
+        speed: 1200,
         adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.png"></button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    dots: true,
+                    arrows: false
+                }
+            }
+        ]
     });
-  });
+});
