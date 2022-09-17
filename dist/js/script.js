154b601b90680ebec821d2ecb237f473ea01c6fd
alex <kasperskiy.94@gmail.com>
2022-09-17 22:25:32 +0300

    seventh day

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,27 +1,12 @@
-let burger = document.querySelector('.header__burger');
-let menu = document.querySelector('.header__list');
+window.addEventListener('DOMContentLoaded', function(){
+    /* Dropdown for math page */
 
-burger.addEventListener('click', function() {
-    burger.classList.toggle('active');
-    menu.classList.toggle('active');
-});
-
-/* Slider */
-
-$(document).ready(function(){
-    $('.carousel__inner').slick({
-        speed: 1200,
-        adaptiveHeight: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.png"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.png"></button>',
-        responsive: [
-            {
-                breakpoint: 992,
-                settings: {
-                    dots: true,
-                    arrows: false
-                }
-            }
-        ]
+    let dropList = document.querySelector('.main__list'),
+        dropBtn = document.querySelector('.main__theme-button'),
+        arrow = document.querySelector('.fa-angle-down');
+    
+    dropBtn.addEventListener('click', function(){
+        dropList.classList.toggle('show');
+        arrow.classList.toggle('rotate');
     });
 });
