99b1d29c7ef720d0e75a1fb16faeb5599e35325a
alex <kasperskiy.94@gmail.com>
2022-09-23 20:54:51 +0300

    ninth day

--- src/js/script.js
+++ src/js/script.js
@@ -1,23 +1,20 @@
-window.addEventListener('DOMContentLoaded', function(){
+window.addEventListener('DOMContentLoaded', function() {
     /* Burger */
 
     const menu = document.querySelector('.header__list'),
-          menuItem = document.querySelector('.burger__span'),
           burger = document.querySelector('.burger');
 
-    burger.addEventListener('click', () => {
+    burger.addEventListener('click', function(){
         burger.classList.toggle('burger__active');
         menu.classList.toggle('header__list__active');
     });
 
     /* Dropdown for math page */
 
-    let dropList = document.querySelector('.main__list'),
-        dropBtn = document.querySelector('.main__theme-button'),
-        arrow = document.querySelector('.fa-angle-down');
-    
+    const dropDown = document.querySelector('.main__list');
+    const dropBtn = document.querySelector('.main__theme-button');
+
     dropBtn.addEventListener('click', function(){
-        dropList.classList.toggle('show');
-        arrow.classList.toggle('rotate');
+        dropDown.classList.toggle('show');
     });
 });
