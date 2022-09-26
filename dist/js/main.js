82dd2f49975b6643e73282cb16e51eb8c38f4e14
alex <kasperskiy.94@gmail.com>
2022-09-26 16:53:57 +0300

    eleventh end day

---
+++ dist/js/main.js
@@ -0,0 +1,113 @@
+window.addEventListener('DOMContentLoaded', function(){
+    /* list */
+
+    const btnFiveFirst = document.querySelector('.main__button-1'),
+        iconFiveFirst = document.querySelector('.arrow-1'),
+        menuFiveFirst = document.querySelector('.main__right-menu-1'),
+        textFirst = document.querySelector('.main__left-text-1');
+
+    btnFiveFirst.addEventListener('click', function(){
+        iconFiveFirst.classList.toggle('rotate');
+        menuFiveFirst.classList.toggle('active');
+        textFirst.classList.toggle('translate');
+    });
+
+    const btnFiveSecond = document.querySelector('.main__button-2'),
+        iconFiveSecond = document.querySelector('.arrow-2'),
+        menuFiveSecond = document.querySelector('.main__right-menu-2'),
+        textSecond = document.querySelector('.main__left-text-2');
+
+    btnFiveSecond.addEventListener('click', function(){
+        iconFiveSecond.classList.toggle('rotate');
+        menuFiveSecond.classList.toggle('active');
+        textSecond.classList.toggle('translate');
+    });
+
+    const btnFiveThird = document.querySelector('.main__button-3'),
+        iconFiveThird = document.querySelector('.arrow-3'),
+        menuFiveThird = document.querySelector('.main__right-menu-3'),
+        textThird = document.querySelector('.main__left-text-3');
+
+    btnFiveThird.addEventListener('click', function(){
+        iconFiveThird.classList.toggle('rotate');
+        menuFiveThird.classList.toggle('active');
+        textThird.classList.toggle('translate');
+    });
+
+    const btnFiveFourth = document.querySelector('.main__button-4'),
+        iconFiveFourth = document.querySelector('.arrow-4'),
+        menuFiveFourth = document.querySelector('.main__right-menu-4'),
+        textFourth = document.querySelector('.main__left-text-4');
+
+    btnFiveFourth.addEventListener('click', function(){
+        iconFiveFourth.classList.toggle('rotate');
+        menuFiveFourth.classList.toggle('active');
+        textFourth.classList.toggle('translate');
+    });
+
+    const btnFiveFifth = document.querySelector('.main__button-5'),
+        iconFiveFifth = document.querySelector('.arrow-5'),
+        menuFiveFifth = document.querySelector('.main__right-menu-5'),
+        textFifth = document.querySelector('.main__left-text-5');
+
+    btnFiveFifth.addEventListener('click', function(){
+        iconFiveFifth.classList.toggle('rotate');
+        menuFiveFifth.classList.toggle('active');
+        textFifth.classList.toggle('translate');
+    });
+
+    const btnFiveSixth = document.querySelector('.main__button-6'),
+        iconFiveSixth = document.querySelector('.arrow-6'),
+        menuFiveSixth = document.querySelector('.main__right-menu-6'),
+        textSixth = document.querySelector('.main__left-text-6');
+
+    btnFiveSixth.addEventListener('click', function(){
+        iconFiveSixth.classList.toggle('rotate');
+        menuFiveSixth.classList.toggle('active');
+        textSixth.classList.toggle('translate');
+    });
+
+    const btnFiveSeventh = document.querySelector('.main__button-7'),
+        iconFiveSeventh = document.querySelector('.arrow-7'),
+        menuFiveSeventh = document.querySelector('.main__right-menu-7'),
+        textSeventh = document.querySelector('.main__left-text-7');
+
+    btnFiveSeventh.addEventListener('click', function(){
+        iconFiveSeventh.classList.toggle('rotate');
+        menuFiveSeventh.classList.toggle('active');
+        textSeventh.classList.toggle('translate');
+    });
+
+    const btnFiveEigth = document.querySelector('.main__button-8'),
+        iconFiveEigth = document.querySelector('.arrow-8'),
+        menuFiveEigth = document.querySelector('.main__right-menu-8'),
+        textEigth = document.querySelector('.main__left-text-8');
+
+    btnFiveEigth.addEventListener('click', function(){
+        iconFiveEigth.classList.toggle('rotate');
+        menuFiveEigth.classList.toggle('active');
+        textEigth.classList.toggle('translate');
+    });
+
+    const btnFiveNinth = document.querySelector('.main__button-9'),
+        iconFiveNinth = document.querySelector('.arrow-9'),
+        menuFiveNinth = document.querySelector('.main__right-menu-9'),
+        textNinth = document.querySelector('.main__left-text-9');
+
+    btnFiveNinth.addEventListener('click', function(){
+        iconFiveNinth.classList.toggle('rotate');
+        menuFiveNinth.classList.toggle('active');
+        textNinth.classList.toggle('translate');
+    });
+
+    const btnFiveTenth = document.querySelector('.main__button-10'),
+        iconFiveTenth = document.querySelector('.arrow-10'),
+        menuFiveTenth = document.querySelector('.main__right-menu-10');
+        textTenth = document.querySelector('.main__left-text-10');
+
+    btnFiveTenth.addEventListener('click', function(){
+        iconFiveTenth.classList.toggle('rotate');
+        menuFiveTenth.classList.toggle('active');
+        textTenth.classList.toggle('translate');
+    });
+});
--- dist/js/script.js
+++ dist/js/script.js
@@ -8,18 +8,4 @@ window.addEventListener('DOMContentLoaded', function() {
         burger.classList.toggle('burger__active');
         menu.classList.toggle('header__list__active');
     });
-
-    /* list */
-
-    const btn = document.querySelector('.main__button'),
-        iconFirst = document.querySelector('.arrow-1'),
-        iconSecond = document.querySelector('.arrow-2'),
-        rightMenu = document.querySelector('.main__right-menu');
-
-    btn.addEventListener('click', function(){
-        iconFirst.classList.toggle('rotate');
-        iconSecond.classList.toggle('rotate');
-        rightMenu.classList.toggle('active');
-    });
-
 });
