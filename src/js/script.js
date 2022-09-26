e4b4e842062993eac44472a5d4370bc14957127f
alex <kasperskiy.94@gmail.com>
2022-09-26 13:32:03 +0300

    eleventh day

--- src/js/script.js
+++ src/js/script.js
@@ -9,4 +9,17 @@ window.addEventListener('DOMContentLoaded', function() {
         menu.classList.toggle('header__list__active');
     });
 
+    /* list */
+
+    const btn = document.querySelector('.main__button'),
+        iconFirst = document.querySelector('.arrow-1'),
+        iconSecond = document.querySelector('.arrow-2'),
+        rightMenu = document.querySelector('.main__right-menu');
+
+    btn.addEventListener('click', function(){
+        iconFirst.classList.toggle('rotate');
+        iconSecond.classList.toggle('rotate');
+        rightMenu.classList.toggle('active');
+    });
+
 });
