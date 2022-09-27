e46bfe1998e9932c3803614552579d1a8031ceb0
alex <kasperskiy.94@gmail.com>
2022-09-27 20:32:08 +0300

    twelvth day

--- dist/js/main.js
+++ dist/js/main.js
@@ -4,7 +4,9 @@ window.addEventListener('DOMContentLoaded', function(){
     const btnFiveFirst = document.querySelector('.main__button-1'),
         iconFiveFirst = document.querySelector('.arrow-1'),
         menuFiveFirst = document.querySelector('.main__right-menu-1'),
-        textFirst = document.querySelector('.main__left-text-1');
+        textFirst = document.querySelector('.main__left-text-1'),
+        buttonFirst = document.querySelector('.hidden__button-01'),
+        arrowFirst = document.querySelector('.arrow-01');
 
     btnFiveFirst.addEventListener('click', function(){
         iconFiveFirst.classList.toggle('rotate');
@@ -12,10 +14,17 @@ window.addEventListener('DOMContentLoaded', function(){
         textFirst.classList.toggle('translate');
     });
 
+    buttonFirst.addEventListener('click', function(){
+        arrowFirst.classList.toggle('rotate');
+        menuFiveFirst.classList.toggle('active');
+    });
+
     const btnFiveSecond = document.querySelector('.main__button-2'),
         iconFiveSecond = document.querySelector('.arrow-2'),
         menuFiveSecond = document.querySelector('.main__right-menu-2'),
-        textSecond = document.querySelector('.main__left-text-2');
+        textSecond = document.querySelector('.main__left-text-2'),
+        buttonSecond = document.querySelector('.hidden__button-02'),
+        arrowSecond = document.querySelector('.arrow-02');
 
     btnFiveSecond.addEventListener('click', function(){
         iconFiveSecond.classList.toggle('rotate');
@@ -23,10 +32,17 @@ window.addEventListener('DOMContentLoaded', function(){
         textSecond.classList.toggle('translate');
     });
 
+    buttonSecond.addEventListener('click', function(){
+        arrowSecond.classList.toggle('rotate');
+        menuFiveSecond.classList.toggle('active');
+    });
+
     const btnFiveThird = document.querySelector('.main__button-3'),
         iconFiveThird = document.querySelector('.arrow-3'),
         menuFiveThird = document.querySelector('.main__right-menu-3'),
-        textThird = document.querySelector('.main__left-text-3');
+        textThird = document.querySelector('.main__left-text-3'),
+        buttonThird = document.querySelector('.hidden__button-03'),
+        arrowThird = document.querySelector('.arrow-03');
 
     btnFiveThird.addEventListener('click', function(){
         iconFiveThird.classList.toggle('rotate');
@@ -34,10 +50,17 @@ window.addEventListener('DOMContentLoaded', function(){
         textThird.classList.toggle('translate');
     });
 
+    buttonThird.addEventListener('click', function(){
+        arrowThird.classList.toggle('rotate');
+        menuFiveThird.classList.toggle('active');
+    });
+
     const btnFiveFourth = document.querySelector('.main__button-4'),
         iconFiveFourth = document.querySelector('.arrow-4'),
         menuFiveFourth = document.querySelector('.main__right-menu-4'),
-        textFourth = document.querySelector('.main__left-text-4');
+        textFourth = document.querySelector('.main__left-text-4'),
+        buttonFourth = document.querySelector('.hidden__button-04'),
+        arrowFourth = document.querySelector('.arrow-04');
 
     btnFiveFourth.addEventListener('click', function(){
         iconFiveFourth.classList.toggle('rotate');
@@ -45,10 +68,17 @@ window.addEventListener('DOMContentLoaded', function(){
         textFourth.classList.toggle('translate');
     });
 
+    buttonFourth.addEventListener('click', function(){
+        arrowFourth.classList.toggle('rotate');
+        menuFiveFourth.classList.toggle('active');
+    });
+
     const btnFiveFifth = document.querySelector('.main__button-5'),
         iconFiveFifth = document.querySelector('.arrow-5'),
         menuFiveFifth = document.querySelector('.main__right-menu-5'),
-        textFifth = document.querySelector('.main__left-text-5');
+        textFifth = document.querySelector('.main__left-text-5'),
+        buttonFifth = document.querySelector('.hidden__button-05'),
+        arrowFifth = document.querySelector('.arrow-05');
 
     btnFiveFifth.addEventListener('click', function(){
         iconFiveFifth.classList.toggle('rotate');
@@ -56,10 +86,17 @@ window.addEventListener('DOMContentLoaded', function(){
         textFifth.classList.toggle('translate');
     });
 
+    buttonFifth.addEventListener('click', function(){
+        arrowFifth.classList.toggle('rotate');
+        menuFiveFifth.classList.toggle('active');
+    });
+
     const btnFiveSixth = document.querySelector('.main__button-6'),
         iconFiveSixth = document.querySelector('.arrow-6'),
         menuFiveSixth = document.querySelector('.main__right-menu-6'),
-        textSixth = document.querySelector('.main__left-text-6');
+        textSixth = document.querySelector('.main__left-text-6'),
+        buttonSixth = document.querySelector('.hidden__button-06'),
+        arrowSixth = document.querySelector('.arrow-06');
 
     btnFiveSixth.addEventListener('click', function(){
         iconFiveSixth.classList.toggle('rotate');
@@ -67,10 +104,18 @@ window.addEventListener('DOMContentLoaded', function(){
         textSixth.classList.toggle('translate');
     });
 
+    buttonSixth.addEventListener('click', function(){
+        arrowSixth.classList.toggle('rotate');
+        menuFiveSixth.classList.toggle('active');
+    });
+
     const btnFiveSeventh = document.querySelector('.main__button-7'),
         iconFiveSeventh = document.querySelector('.arrow-7'),
         menuFiveSeventh = document.querySelector('.main__right-menu-7'),
-        textSeventh = document.querySelector('.main__left-text-7');
+        textSeventh = document.querySelector('.main__left-text-7'),
+        buttonSeventh = document.querySelector('.hidden__button-07'),
+        arrowSeventh = document.querySelector('.arrow-07');
+
 
     btnFiveSeventh.addEventListener('click', function(){
         iconFiveSeventh.classList.toggle('rotate');
@@ -78,10 +123,17 @@ window.addEventListener('DOMContentLoaded', function(){
         textSeventh.classList.toggle('translate');
     });
 
+    buttonSeventh.addEventListener('click', function(){
+        arrowSeventh.classList.toggle('rotate');
+        menuFiveSeventh.classList.toggle('active');
+    });
+
     const btnFiveEigth = document.querySelector('.main__button-8'),
         iconFiveEigth = document.querySelector('.arrow-8'),
         menuFiveEigth = document.querySelector('.main__right-menu-8'),
-        textEigth = document.querySelector('.main__left-text-8');
+        textEigth = document.querySelector('.main__left-text-8'),
+        buttonEigth = document.querySelector('.hidden__button-08'),
+        arrowEigth = document.querySelector('.arrow-08');
 
     btnFiveEigth.addEventListener('click', function(){
         iconFiveEigth.classList.toggle('rotate');
@@ -89,10 +141,17 @@ window.addEventListener('DOMContentLoaded', function(){
         textEigth.classList.toggle('translate');
     });
 
+    buttonEigth.addEventListener('click', function(){
+        arrowEigth.classList.toggle('rotate');
+        menuFiveEigth.classList.toggle('active');
+    });
+
     const btnFiveNinth = document.querySelector('.main__button-9'),
         iconFiveNinth = document.querySelector('.arrow-9'),
         menuFiveNinth = document.querySelector('.main__right-menu-9'),
-        textNinth = document.querySelector('.main__left-text-9');
+        textNinth = document.querySelector('.main__left-text-9'),
+        buttonNinth = document.querySelector('.hidden__button-09'),
+        arrowNinth = document.querySelector('.arrow-09');
 
     btnFiveNinth.addEventListener('click', function(){
         iconFiveNinth.classList.toggle('rotate');
@@ -100,14 +159,26 @@ window.addEventListener('DOMContentLoaded', function(){
         textNinth.classList.toggle('translate');
     });
 
+    buttonNinth.addEventListener('click', function(){
+        arrowNinth.classList.toggle('rotate');
+        menuFiveNinth.classList.toggle('active');
+    });
+
     const btnFiveTenth = document.querySelector('.main__button-10'),
         iconFiveTenth = document.querySelector('.arrow-10'),
         menuFiveTenth = document.querySelector('.main__right-menu-10');
-        textTenth = document.querySelector('.main__left-text-10');
+        textTenth = document.querySelector('.main__left-text-10'),
+        buttonTenth = document.querySelector('.hidden__button-010'),
+        arrowTenth = document.querySelector('.arrow-010');
 
     btnFiveTenth.addEventListener('click', function(){
         iconFiveTenth.classList.toggle('rotate');
         menuFiveTenth.classList.toggle('active');
         textTenth.classList.toggle('translate');
+    });
+
+    buttonTenth.addEventListener('click', function(){
+        arrowTenth.classList.toggle('rotate');
+        menuFiveTenth.classList.toggle('active');
     });
 });
